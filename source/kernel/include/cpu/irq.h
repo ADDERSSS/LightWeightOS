d03e3638fe0077ab1913070b536db3f030b47fbf
Su <66754980@bupt.edu.cn>
2025-01-20 00:50:32 +0800

    Resolve_exceptions

--- source/kernel/include/cpu/irq.h
+++ source/kernel/include/cpu/irq.h
@@ -5,6 +5,14 @@
 #include "comm/cpu_instr.h"
 #include "os_cfg.h"
 
+typedef struct _exception_frame_t {
+    uint32_t gs, fs, es, ds;
+    uint32_t edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    uint32_t eip, cs, eflags;
+
+
+}exception_frame_t;
+
 void irq_init (void);
 
 #endif
